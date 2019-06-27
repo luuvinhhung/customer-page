e2aa936c4e4713121aa10802676c27d80b3ccc61
LuuVinhHung <37299630+luuvinhhung@users.noreply.github.com>
2019-06-27 17:13:57 +0700

    Add Tabs + Scrolltoview

--- src/index.js
+++ src/index.js
@@ -50,11 +50,13 @@ class DynamicRule extends React.Component {
       }
     );
   };
-  myFunction(x) {
-    console.log("ok");
-    var elmnt = document.getElementById(`${x}`);
+
+  myFunction(e) {
+    console.log(e);
+    var elmnt = document.getElementById(e);
     elmnt.scrollIntoView({ behavior: "smooth", block: "center" });
   }
+
   render() {
     let mobile = "123";
     let address = "1234";
@@ -114,273 +116,57 @@ class DynamicRule extends React.Component {
         </div>
 
         <div>
-          <Tabs defaultActiveKey="1" className="tabIn">
-            <TabPane tab="Thông tin cá nhân" key="1" className="tabIn1">
-              <div>
-                <Form {...formItemLayout} labelAlign="left">
-                  <Form.Item label="mobile">
-                    {getFieldDecorator("mobile", {
-                      rules: [
-                        {
-                          pattern: /^[^0]/,
-                          message: "chữ cái đầu là 0"
-                        },
-                        {
-                          pattern: /(?!^[0-9]{10,11})/,
-                          message: "từ 10 tới 11 chữ số"
-                        }
-                      ],
-                      initialValue: mobile
-                    })(
-                      <Input
-                        className="inp"
-                        placeholder="số điện thoại"
-                        type={"text"}
-                      />
-                    )}
-                  </Form.Item>
-
-                  <Form.Item label="giới tính ">
-                    <RadioGroup>
-                      <Radio value="male">Nam</Radio>
-                      <Radio value="female">Nữ</Radio>
-                    </RadioGroup>
-                  </Form.Item>
-
-                  <Form.Item label="Ngày sinh">
-                    <InputGroup>
-                      <DatePicker className="dayinp" />
-                    </InputGroup>
-                  </Form.Item>
-                  <Form.Item label="cân nặng ">
-                    {getFieldDecorator("weight", {
-                      initialValue: weight
-                    })(<InputNumber placeholder="cân nặng" className="inp " />)}
-                  </Form.Item>
-
-                  <Form.Item label="tên người giám hộ">
-                    {getFieldDecorator("guardian", {
-                      rules: [
-                        {
-                          required: true,
-                          message: "Name can not be blank!"
-                        },
-                        {
-                          pattern: /^[^\s]+/,
-                          message: "No white space at beginning"
-                        },
-                        {
-                          pattern: /^[^!@#$%^&*()+\-=\[\]{};':'\\|,.<>\/?]+$/,
-                          message: "Do not contains special characters"
-                        }
-                      ],
-                      initialValue: guardian
-                    })(
-                      <Input placeholder="tên người giám hộ" className="inp " />
-                    )}
-                  </Form.Item>
-                  <Form.Item label="CMND ">
-                    {getFieldDecorator("guardianID", {
-                      initialValue: guardianID
-                    })(<Input placeholder="guardianID" className="inp" />)}
-                  </Form.Item>
-
-                  <Form.Item label="địa chỉ">
-                    {getFieldDecorator("address", {
-                      initialValue: address
-                    })(<Input placeholder="Địa chỉ" className="inp" />)}
-                  </Form.Item>
-
-                  <Form.Item label="email">
-                    {getFieldDecorator("email", {
-                      rules: [
-                        {
-                          type: "email",
-                          message: "The input is not valid E-mail!"
-                        },
-                        {
-                          required: true,
-                          message: "Please input your E-mail!"
-                        }
-                      ],
-                      initialValue: email
-                    })(<Input placeholder="Email" className="inp" />)}
-                  </Form.Item>
-                </Form>
-              </div>
-            </TabPane>
+          <Tabs defaultActiveKey="2" onChange={this.myFunction}>
             <TabPane
-              tab="Thông tin nghề nghiệp"
-              key="2"
-              className="tabIn2"
-              onClick={this.myFunction}
-            >
-              <Form {...formItemLayout}>
-                <Form.Item label="số thẻ BHYT">
-                  {getFieldDecorator("healthInsuranceNumber", {
-                    initialValue: healthInsuranceNumber
-                  })(
-                    <Input
-                      placeholder="số thẻ BHYT"
-                      type={"text"}
-                      className="inp"
-                    />
-                  )}
-                </Form.Item>
-
-                <Form.Item label="hạn thẻ BHYT">
-                  {getFieldDecorator("healthInsuranceExpired", {
-                    initialValue: healthInsuranceExpired
-                  })(
-                    <InputGroup>
-                      <DatePicker placeholder="dd/mm/yy" className="dayinp" />
-                    </InputGroup>
-                  )}
-                </Form.Item>
-                <Form.Item label="nghề nghiệp">
-                  {getFieldDecorator("jobName", {
-                    initialValue: jobName
-                  })(
-                    <Input
-                      className="inp"
-                      placeholder="nhập nghề nghiệp"
-                      type={"text"}
-                    />
-                  )}
-                </Form.Item>
-
-                <Form.Item label="chức vụ">
-                  {getFieldDecorator("positionName", {
-                    initialValue: positionName
-                  })(
-                    <Input
-                      className="inp"
-                      placeholder="nhập chức vụ"
-                      type={"text"}
-                    />
-                  )}
-                </Form.Item>
-
-                <Form.Item label="cơ quan">
-                  {getFieldDecorator("companyName", {
-                    initialValue: companyName
-                  })(
-                    <Input
-                      className="inp"
-                      placeholder="nhập công ty"
-                      type={"text"}
-                    />
-                  )}
-                </Form.Item>
-              </Form>
-            </TabPane>
-
-            <TabPane tab="Bệnh sử y khoa" key="3" className="tabIn3">
-              <Form {...formItemLayout}>
-                <Form.Item label="bệnh sử y khoa" className="mH">
-                  {getFieldDecorator("medicalHistory", {
-                    initialValue: medicalHistory
-                  })(
-                    <RadioGroup>
-                      <Radio value={1}>khỏe mạnh</Radio>
-                      <Radio value={2}>không rõ</Radio>
-                    </RadioGroup>
-                  )}
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="isHealthBloodPressure">
-                    bệnh lý tim mạch, cao huyết áp
-                  </Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="isInfectiousDiseases">
-                    Bệnh truyền nhiễm (Lao, HIV, HBV,...)
-                  </Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="isDiabetes">Tiểu đường</Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="isProlongedBleeding">
-                    chảy máy kéo dài
-                  </Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="isStomachAche">đau bao tử</Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="isBothKidney">bênh lý gan, thận</Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="isPregnant">đang có thai</Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="isAllergyAsthma">dị ứng, suyễn</Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="isTakingMedicine">đang dùng thuốc</Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Checkbox name="otherDiseases">bệnh khác</Checkbox>
-                </Form.Item>
-
-                <Form.Item {...formTailLayout}>
-                  <Input
-                    placeholder="Nhập bệnh sử khác"
-                    className="inpOtherDis"
-                    type={"text"}
-                  />
-                </Form.Item>
-              </Form>
-            </TabPane>
-
-            <div>
-              <Upload
-                listType="picture-card"
-                className="upload-picture"
-                onChange={this.readFile}
-              >
-                {uploadButton}
-              </Upload>
-            </div>
+              tab={
+                <span>
+                  <Icon type="apple" />1
+                </span>
+              }
+              key="qwer"
+            />
+            <TabPane
+              tab={
+                <span>
+                  <Icon type="apple" />2
+                </span>
+              }
+              key="asdf"
+            />
+            <TabPane
+              tab={
+                <span>
+                  <Icon type="apple" />3
+                </span>
+              }
+              key="zxcv"
+            />
           </Tabs>
-        </div>
-        <ul>
-          <li>
-            <p onClick={() => this.myFunction("qwer")}>1</p>
-          </li>
-          <li>
-            <p onClick={() => this.myFunction("asdf")}>2</p>
-          </li>
-          <li>
-            <p onClick={() => this.myFunction("zxcv")}>2</p>
-          </li>
-        </ul>
-        <div
-          id="qwer"
-          style={{ height: 500, border: "black 1px solid", marginBottom: 20 }}
-        >
-          asdfasfd
-        </div>
-        <div
-          id="asdf"
-          style={{ height: 500, border: "red 1px solid", marginBottom: 20 }}
-        />
 
-        <div
-          id="zxcv"
-          style={{ height: 500, border: "green 1px solid", marginBottom: 20 }}
-        />
+          {/* <ul>
+            <li>
+              <p onClick={() => this.myFunction("qwer")}>1</p>
+            </li>
+            <li>
+              <p onClick={() => this.myFunction("asdf")}>2</p>
+            </li>
+            <li>
+              <p onClick={() => this.myFunction("zxcv")}>2</p>
+            </li>
+          </ul> */}
+          <div
+            id="qwer"
+            style={{ height: 500, border: "black 1px solid", marginBottom: 20 }}
+          />
+          <div
+            id="asdf"
+            style={{ height: 500, border: "red 1px solid", marginBottom: 20 }}
+          />
+          <div
+            id="zxcv"
+            style={{ height: 500, border: "green 1px solid", marginBottom: 20 }}
+          />
+        </div>
       </div>
     );
   }
